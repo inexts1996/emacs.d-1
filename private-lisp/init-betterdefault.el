f0275f8bf8d024e5c34279148478c35b96009448
inextsEthereal <inexts.ethereal@gmail.com>
2020-03-03 20:10:21 +0800

    add private config

---
+++ private-lisp/init-betterdefault.el
@@ -0,0 +1,8 @@
+ (load-theme 'sanityinc-tomorrow-bright t)
+(set-frame-font "Ubuntu Mono-13" nil t)
+(setq make-backup-files nil)
+;; stop creating those #auto-save# files
+(setq auto-save-default nil)
+(setq delete-trailing-whitespace t)
+
+(provide 'init-inexts-betterdefault)
---
+++ private-lisp/init-packages.el
@@ -0,0 +1,34 @@
+;;(when (>= emacs-major-version 24)
+;;   (require 'package)
+;;     (package-initialize)
+;;    (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+;;		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+
+ ;; cl - Common Lisp Extension
+ (require 'cl)
+
+ ;; Add Packages
+ (defvar my/packages '(
+    csharp-mode
+    omnisharp
+		) "Default packages")
+
+ (setq package-selected-packages my/packages)
+
+ (defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+	   when (not (package-installed-p pkg)) do (return nil)
+	   finally (return t)))
+
+ (unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+	 (package-install pkg))))
+
+ ;; Find Executable Path on OS X
+ (when (memq window-system '(mac ns))
+   (exec-path-from-shell-initialize))
+(provide 'init-packages)

4622a5a6725fead9d3ecda447fc76ff4ad21e962
inextsEthereal <inexts.ethereal@gmail.com>
2020-03-05 00:41:34 +0800

    add init-recentf.el

--- private-lisp/init-betterdefault.el
+++ private-lisp/init-betterdefault.el
@@ -4,5 +4,7 @@
 ;; stop creating those #auto-save# files
 (setq auto-save-default nil)
 (setq delete-trailing-whitespace t)
+;; highlight current line
+(global-hl-line-mode t)
 
 (provide 'init-inexts-betterdefault)
